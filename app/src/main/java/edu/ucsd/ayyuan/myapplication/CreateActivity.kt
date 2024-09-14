0258d78a1d1a72db2ea8567d1c3f0dd5555fae5d
Ayden Yuan <ayyuan@ucsd.edu>
2024-09-14 00:25:25 -0700

    Improve activity creation screen

--- app/src/main/java/edu/ucsd/ayyuan/myapplication/CreateActivity.kt
+++ app/src/main/java/edu/ucsd/ayyuan/myapplication/CreateActivity.kt
@@ -36,7 +36,7 @@ class CreateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.create_activity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
